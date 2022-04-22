39d2c3f20486ea3765d1dda9d18f8c9615baff46
Mohammed Ajlan <65132450+Moh248@users.noreply.github.com>
2022-04-23 00:38:47 +0300

    Find Average

---
+++ Average.py
@@ -0,0 +1,10 @@
+
+#---Write a function that receives an array of integers and returns the arithmetic average of these integers in the array---
+
+def average(array):
+  # write your code here
+ return print(sum(array)/len(array))
+
+pass
+
+
